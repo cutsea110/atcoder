c710a6cbf3308e4a248c01bdfef93a796fcd3f3b
cutsea110 <cutsea110@gmail.com>
2019-06-29 19:56:19 +0900

    [Refactor]

--- ABC081/PlacingMarbles.hs
+++ ABC081/PlacingMarbles.hs
@@ -1,8 +1,4 @@
 module PlacingMarbles where
 
-import Data.Char (ord)
-
 main :: IO ()
-main = do
-  xs <- getLine
-  print $ foldr (\c s -> (ord c - ord '0') + s) 0 xs
+main = print . length . filter (=='1') =<< getLine
