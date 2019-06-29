d943d6d4dbd2714a59fb9bb3d9bfccede4e05c37
cutsea110 <cutsea110@gmail.com>
2019-06-29 20:04:21 +0900

    [Refactor]

--- ABC083/SomeSums.hs
+++ ABC083/SomeSums.hs
@@ -1,19 +1,17 @@
 module Main where
 
 import Data.List (unfoldr)
+import Data.Tuple (swap)
 
 toDigits :: Int -> [Int]
 toDigits = unfoldr phi
   where
-    swap (x,y) = (y,x)
-    phi n | n == 0    = Nothing
-          | otherwise = Just (swap (n `divMod` 10))
+    phi n = if n /= 0 then Just (swap (n `divMod` 10)) else Nothing
 
-calc :: Int -> Int -> Int -> Int
-calc n a b = sum $ filter p [1..n]
+calc :: [Int] -> Int
+calc [n,a,b] = sum $ filter p [1..n]
   where
     p m = a <= s && s <= b where s = sum (toDigits m)
 
-main = do
-  [n,a,b] <- (fmap read . words) <$> getLine
-  print (calc n a b)
+main :: IO ()
+main = print . calc =<< fmap read . words <$> getLine
