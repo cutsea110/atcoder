ffe70dce1980d3c9e99f1d440ecbc78dfee7817d
cutsea110 <cutsea110@gmail.com>
2019-06-29 20:09:09 +0900

    [Refactor]

--- ABC088/CardGameForTwo.hs
+++ ABC088/CardGameForTwo.hs
@@ -5,9 +5,9 @@ import Data.List
 
 sortDesc = sortBy (flip compare)
 
+main :: IO ()
 main = do
   n <- readLn
-  xs <- (map read . words) <$> getLine
+  xs <- map read . words <$> getLine
   let (alice, bob) = (map snd *** map snd) $ partition (odd . fst) $ zip [1..n] $ sortDesc xs
   print $ sum alice - sum bob
-      
