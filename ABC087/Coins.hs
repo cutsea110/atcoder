058364a673ea118125d5e0e500ee60312b424c27
cutsea110 <cutsea110@gmail.com>
2019-06-29 20:02:49 +0900

    [Refactor]

--- ABC087/Coins.hs
+++ ABC087/Coins.hs
@@ -1,10 +1,9 @@
 module Main where
 
-calc :: (Int, Int, Int, Int) -> Int
-calc (a, b, c, x) = sum [1| a'<-[0..a], b'<-[0..b], c'<-[0..c], 500*a'+100*b'+50*c' == x]
-                        
-main :: IO ()
-main = do
-  ps <- (,,,) <$> readLn <*> readLn <*> readLn <*> readLn
-  print $ calc ps
+import Control.Monad (replicateM)
 
+calc :: [Int] -> Int
+calc [a, b, c, x] = sum [1| a'<-[0..a], b'<-[0..b], c'<-[0..c], 500*a'+100*b'+50*c' == x]
+
+main :: IO ()
+main = print . calc =<< replicateM 4 readLn
