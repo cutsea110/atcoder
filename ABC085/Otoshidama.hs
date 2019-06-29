12104e627a6ae5f63cf83d73cf74b40a7335b14f
cutsea110 <cutsea110@gmail.com>
2019-06-29 20:16:21 +0900

    [Refactor]

--- ABC085/Otoshidama.hs
+++ ABC085/Otoshidama.hs
@@ -1,13 +1,11 @@
 module Main where
 
-import Text.Printf (printf)
-
-pat :: Int -> Int -> [(Int, Int, Int)]
-pat n y = [(a,b,c)| a <- [0..n], b <- [0..n-a], let c = n - a - b, 10000*a+5000*b+1000*c == y]
+pat :: Int -> Int -> [[Int]]
+pat n y = [[a,b,c]| a <- [0..n], b <- [0..n-a], let c = n - a - b, 10000*a+5000*b+1000*c == y]
 
 main :: IO ()
 main = do
-  [n,y] <- (map read . words) <$> getLine
+  [n,y] <- map read . words <$> getLine
   let ps = pat n y
-  let (a,b,c) = if null ps then (-1,-1,-1) else head ps
-  printf "%d %d %d\n" a b c
+  let xs = if null ps then [-1,-1,-1] else head ps
+  putStrLn $ unwords $ map show xs
