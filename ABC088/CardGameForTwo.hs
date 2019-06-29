3355e55094e6da3eca58cf5c476d507422a31012
cutsea110 <cutsea110@gmail.com>
2019-06-29 15:20:00 +0900

    clear Card Game for Two

---
+++ ABC088/CardGameForTwo.hs
@@ -0,0 +1,13 @@
+module Main where
+
+import Control.Arrow ((***))
+import Data.List
+
+sortDesc = sortBy (flip compare)
+
+main = do
+  n <- readLn
+  xs <- (map read . words) <$> getLine
+  let (alice, bob) = (map snd *** map snd) $ partition (odd . fst) $ zip [1..n] $ sortDesc xs
+  print $ sum alice - sum bob
+      
