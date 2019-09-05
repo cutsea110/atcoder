f1b92e87974aba63d7805d4cadb7607f42acbc61
cutsea110 <cutsea110@gmail.com>
2019-09-05 18:25:30 +0900

    refactor

--- ABC139/League.hs
+++ ABC139/League.hs
@@ -1,9 +1,15 @@
+{-# OPTIONS_GHC -O2 #-}
+{-# LANGUAGE BangPatterns #-}
 module Main where
 
 import Control.Monad
 import Data.Maybe
 import qualified Data.Vector.Unboxed as V
 import qualified Data.ByteString.Char8 as BS
+
+fst3 (x,_,_) = x
+snd3 (_,y,_) = y
+thd3 (_,_,z) = z
 
 tuplify2 (x:y:_) = (x,y)
 tuplify2 _ = undefined
@@ -27,29 +33,29 @@ main = do
   ex <- getIntNList n
   print $ simulate 0 (ini ex)
 
-ini :: [[Int]] -> [(Int, (Int, [Int]))]
-ini xss = zipWith (\x i -> (i, (head' x, tail' x))) xss [1..]
+ini :: [[Int]] -> [(Int, (Int, [Int], State))]
+ini xss = zipWith (\x i -> (i, split' x)) xss [1..]
 
-step :: [(Int, (Int, [Int]))] -> [(Int, (Int, [Int]))]
-step ihts = bs
+data State = Progress | Keep | Done deriving (Show, Eq, Ord)
+
+step :: [(Int, (Int, [Int], State))] -> [(Int, (Int, [Int], State))]
+step ihtbs = zipWith f (V.toList hs') ihtbs
   where
-    hs = V.fromList $ map (fst.snd) ihts
+    hs = V.fromList $ map (fst3.snd) ihtbs
     hs' = V.map (\h -> if h == 0 then 0 else hs V.! (h-1)) hs
-    bs = zipWith (\h' (i, (h, t)) -> if h' == i then (i, (head' t, tail' t)) else (i, (h, t))) (V.toList hs') ihts
+    f h' !(i, (h, !t, s)) | h' == i = (i, split' t)
+                          | otherwise = (i, (h, t, max Keep s))
 
-simulate :: Int -> [(Int, (Int, [Int]))] -> Int
-simulate n ex | ex == ex' = -1
-              | all (((0,[])==).snd) ex' = n+1
-              | otherwise = simulate (n+1) ex'
-  where ex' = step ex
+simulate :: Int -> [(Int, (Int, [Int], State))] -> Int
+simulate n ex | any ((Progress==).thd3.snd) ex' = simulate (n+1) ex'
+              | all ((Done==).thd3.snd) ex' = n+1
+              | otherwise = -1
+  where !ex' = step ex
+    
 
-head' :: [Int] -> Int
-head' [] = 0
-head' (x:xs) = x
-
-tail' :: [Int] -> [Int]
-tail' [] = []
-tail' (_:xs) = xs
+split' :: [Int] -> (Int, [Int], State)
+split' [] = (0, [], Done)
+split' (x:xs) = (x, xs, Progress)
 
 ex1 :: [[Int]]
 ex1 = [[2,3],[1,3],[1,2]]
@@ -59,7 +65,6 @@ ex2 = [[2,3,4],[1,3,4],[4,1,2],[3,1,2]]
 
 ex3 :: [[Int]]
 ex3 = [[2,3],[3,1],[1,2]]
-
 
 -- ex.1
 -- 3
