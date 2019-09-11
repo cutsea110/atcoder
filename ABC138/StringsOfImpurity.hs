171bb2471e9baf07b27e070b4276a6c86f33ae9b
cutsea110 <cutsea110@gmail.com>
2019-09-11 10:29:24 +0900

    trying...

---
+++ ABC138/StringsOfImpurity.hs
@@ -0,0 +1,106 @@
+{-# OPTIONS_GHC -O2 #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BangPatterns #-}
+module Main where
+
+import Control.Arrow
+import Control.Monad
+import Control.Monad.ST
+import Control.Monad.State.Strict
+import Data.Char
+import Data.Function
+import Data.Graph
+import Data.List
+import Data.Maybe
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as VM
+import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed.Mutable as UM
+import qualified Data.ByteString.Char8 as C
+import qualified Data.ByteString.Unsafe as B
+import qualified Data.ByteString.Builder as BSB
+import System.IO (stdout)
+
+type Parser a = C.ByteString -> Maybe (a, C.ByteString)
+
+parseInt :: Parser Int
+parseInt = C.readInt . C.dropWhile isSpace
+
+parseInt2 :: Parser (Int, Int)
+parseInt2 = runStateT $
+    (,) <$> StateT (C.readInt . C.dropWhile isSpace)
+        <*> StateT (C.readInt . B.unsafeTail)
+ 
+parseInt3 :: Parser (Int, Int, Int)
+parseInt3 = runStateT $
+    (,,) <$> StateT (C.readInt . C.dropWhile isSpace)
+        <*> StateT (C.readInt . B.unsafeTail)
+        <*> StateT (C.readInt . B.unsafeTail)
+ 
+parseInt4 :: Parser (Int, Int, Int, Int)
+parseInt4 = runStateT $
+    (,,,) <$> StateT (C.readInt . C.dropWhile isSpace)
+        <*> StateT (C.readInt . B.unsafeTail)
+        <*> StateT (C.readInt . B.unsafeTail)
+        <*> StateT (C.readInt . B.unsafeTail)
+
+-- prime means pred
+parseInt' :: Parser Int
+parseInt' = fmap (pred *** id) . parseInt
+
+getInts :: IO [Int]
+getInts = unfoldr parseInt <$> C.getLine
+
+-- prime means pred
+getInts' :: IO [Int]
+getInts' = unfoldr parseInt' <$> C.getLine
+
+getIntTuple :: IO (Int, Int)
+getIntTuple = do
+  a:b:_ <- getInts
+  return (a, b)
+
+-- prime means pred
+getIntTuple' :: IO (Int, Int)
+getIntTuple' = do
+  a:b:_ <- getInts'
+  return (a, b)
+
+getIntTuple3 :: IO (Int, Int, Int)
+getIntTuple3 = do
+  a:b:c:_ <- getInts
+  return (a, b, c)
+
+getIntTuple4 :: IO (Int, Int, Int, Int)
+getIntTuple4 = do
+  a:b:c:d:_ <- getInts
+  return (a, b, c, d)
+
+getIntVec :: Int -> IO (U.Vector Int)
+getIntVec n = U.unfoldrN n parseInt <$> C.getLine
+
+-- prime means pred
+getIntVec' :: Int -> IO (U.Vector Int)
+getIntVec' n = U.unfoldrN n parseInt' <$> C.getLine
+
+---------------------------------------------------------
+
+main :: IO ()
+main = do
+  s <- C.getLine
+  t <- C.getLine
+  print (match s t (C.length s) 0 0)
+
+
+type Count = Integer
+type Length = Int
+type Index = Int
+
+match :: C.ByteString -> C.ByteString -> Length -> Index -> Count -> Count
+match s !t !l !i !c | C.null t = c
+                    | c >= lim = -1
+                    | C.index s i == C.head t = match s (C.tail t) l ((i+1) `mod` l) (c+1)
+                    | otherwise               = match s t          l ((i+1) `mod` l) (c+1)
+  where
+    lim :: Integer
+    lim = fromIntegral (l*100)
