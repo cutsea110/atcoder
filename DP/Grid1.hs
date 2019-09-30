60f727f0899c1c87b7785499b5291306a2f9bb37
cutsea110 <cutsea110@gmail.com>
2019-09-30 15:50:12 +0900

    solve Grid1

---
+++ DP/Grid1.hs
@@ -0,0 +1,148 @@
+{-# OPTIONS_GHC -O2 #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+module Main where
+
+import Control.Monad (replicateM, forM_)
+import Data.Bool (bool)
+import Data.Char (isSpace)
+import Data.Function (on)
+import Data.Graph (Vertex, Edge, buildG, topSort)
+import Data.List (unfoldr, foldl', sort, (\\), delete)
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed.Mutable as UM
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as VM
+import qualified Data.ByteString.Char8 as C
+import qualified Data.ByteString.Unsafe as B
+import System.IO (hPutStr, hPutStrLn, stdin, stdout, withFile, IOMode(..))
+
+type Parser a = C.ByteString -> Maybe (a, C.ByteString)
+
+parseInt :: Parser Int
+parseInt = C.readInt . C.dropWhile isSpace
+
+getInts :: IO [Int]
+getInts = unfoldr parseInt <$> C.getLine
+
+getIntVec :: Int -> IO (U.Vector Int)
+getIntVec n = U.unfoldrN n parseInt <$> C.getLine
+
+---------------------------------------------------------
+swap (x, y) = (y, x)
+pair (f, g) x = (f x, g x)
+cross (f, g) (x, y) = (f x, g y)
+
+newtype Fix f = In { out :: f (Fix f) }
+
+newtype Cofree f a = Cf { unCf :: (a, f (Cofree f a)) }
+extract :: Cofree f t -> t
+extract = fst . unCf
+sub :: Functor f => Cofree f a -> f (Cofree f a)
+sub = snd . unCf
+
+newtype Free f a = Fr { unFr :: Either a (f (Free f a)) }
+inject :: a -> Free f a
+inject = Fr . Left
+
+-- catamorphism
+cata :: Functor f => (f a -> a) -> Fix f -> a
+cata phi = phi . fmap (cata phi) . out
+-- anamorphism
+ana :: Functor f => (a -> f a) -> a -> Fix f
+ana psi = In . fmap (ana psi) . psi
+-- hylomorphism
+hylo :: Functor f => (f b -> b) -> (a -> f a) -> a -> b
+hylo phi psi = phi . fmap (hylo phi psi) . psi -- cata phi . ana psi
+-- metamorphism
+meta :: Functor f => (f a -> a) -> (a -> f a) -> Fix f -> Fix f
+meta phi psi = In . fmap (meta phi psi) . out -- ana psi . cata phi
+-- paramorphism
+para :: Functor f => (f (Fix f, t) -> t) -> Fix f -> t
+para phi = phi . fmap (pair (id, para phi)) . out
+-- apomorphism
+apo :: Functor f => (t -> f (Either (Fix f) t)) -> t -> Fix f
+apo psi = In . fmap (uncurry either (id, apo psi)) . psi
+-- histomorphism
+histo :: Functor f => (f (Cofree f t) -> t) -> Fix f -> t
+histo phi = extract . cata (Cf . pair (phi, id))
+-- futumorphism
+futu :: Functor f => (t -> f (Free f t)) -> t -> Fix f
+futu psi = ana (uncurry either (psi, id) . unFr) . inject
+-- chronomorphism
+chrono :: Functor f => (f (Cofree f b) -> b) -> (a -> f (Free f a)) -> a -> b
+chrono phi psi = extract . hylo phi' psi' . inject
+  where
+    phi' = Cf . pair (phi, id)
+    psi' = uncurry either (psi, id) . unFr
+-- cochronomorphism
+cochrono :: Functor f => (f (Cofree f t) -> t) -> (t -> f (Free f t)) -> Fix f -> Fix f
+cochrono phi psi = futu psi . histo phi
+-- zygomorphism
+zygo :: Functor f => (f a -> a) -> (f (a, b) -> b) -> Fix f -> b
+zygo f phi = snd . cata (pair (f . fmap fst, phi))
+-- cozygomorphism
+cozygo :: Functor f => (a -> f a) -> (b -> f (Either a b)) -> b -> Fix f
+cozygo f psi = ana (uncurry either (fmap Left . f, psi)) . Right
+-- dynamorphism
+dyna :: Functor f => (f (Cofree f b) -> b) -> (a -> f a) -> a -> b
+dyna f g = chrono f (fmap inject . g) -- histo f . ana g
+-- codynamorphism
+codyna :: Functor f => (f b -> b) -> (a -> f (Free f a)) -> a -> b
+codyna f g = chrono (f . fmap extract) g
+-- mutumorphism
+mutu :: Functor f => (a -> b) -> (f a -> a) -> Fix f -> b
+mutu proj phi = proj . cata phi
+-- comutumorphism
+comutu :: Functor f => (b -> a) -> (a -> f a) -> b -> Fix f
+comutu proj psi = ana psi . proj
+
+
+foldn (c, f) 0 = c
+foldn (c, f) n = f (foldn (c, f) (n-1))
+
+paran (c, f) 0 = c
+paran (c, f) n = f n (paran (c, f) (n-1))
+
+---------------------------------------------------------
+
+getTuple :: IO Edge
+getTuple = do
+  (x:y:_) <- getInts
+  return (x, y)
+
+data NonEmptyListF a = NonEmptyListF (Char, (Int, Int)) (Maybe a) deriving (Show, Functor)
+
+main :: IO ()
+main = do
+  (h, w) <- getTuple
+  bs <- C.concat <$> replicateM h C.getLine
+  print $ solve (h, w) bs
+
+solve (h, w) bs = dyna phi psi (h*w-1)
+  where
+    lim = 10^9+7
+    
+    pos x = (x `div` w, x `mod` w)
+    
+    psi 0 = NonEmptyListF (bs `C.index` 0, pos 0) Nothing
+    psi i = NonEmptyListF (bs `C.index` i, pos i) (Just (i-1))
+
+--     phi :: NonEmptyListF (Cofree NonEmptyListF Int) -> Int
+    phi (NonEmptyListF (_, (i, j)) Nothing) = 1
+    phi prev@(NonEmptyListF ('#', (i, j)) (Just t)) = 0
+    phi prev@(NonEmptyListF ('.', (i, j)) (Just t))
+      | i == 0 = back 1 prev
+      | j == 0 = back w prev
+      | otherwise = (back 1 prev + back w prev) `mod` lim
+
+    back 1 (NonEmptyListF _ (Just t)) = extract t
+    back i (NonEmptyListF _ (Just t)) = case sub t of
+      NonEmptyListF _ Nothing -> 1
+      prev@(NonEmptyListF _ (Just t')) -> back (i-1) prev
