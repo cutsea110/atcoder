81e43332b0a849022eb45dec7f907a3c7a310421
cutsea110 <cutsea110@gmail.com>
2020-04-15 11:10:02 +0900

    solved FizzBuzzSum

---
+++ ABC162/FizzBuzzSum.hs
@@ -0,0 +1,48 @@
+{-# OPTIONS_GHC -O2 #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NPlusKPatterns #-}
+module Main where
+
+import Data.List (unfoldr)
+
+import Control.Monad (replicateM, forM_)
+import Data.Bool (bool)
+import Data.Char (isSpace)
+import Data.Function (on)
+import qualified Data.Foldable as Foldable
+import Data.List (unfoldr, foldl', sort, (\\), delete, nub)
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed.Mutable as UM
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as VM
+import qualified Data.ByteString.Char8 as C
+import qualified Data.ByteString.Unsafe as B
+import System.IO (hPutStr, hPutStrLn, stdin, stdout, withFile, IOMode(..))
+
+type Parser a = C.ByteString -> Maybe (a, C.ByteString)
+
+parseInt :: Parser Int
+parseInt = C.readInt . C.dropWhile isSpace
+
+getInts :: IO [Int]
+getInts = unfoldr parseInt <$> C.getLine
+
+getIntVec :: Int -> IO (U.Vector Int)
+getIntVec n = U.unfoldrN n parseInt <$> C.getLine
+
+fizzbuzz n = filter p [1..n]
+  where p i = i `mod` 3 /= 0 && i `mod` 5 /= 0
+
+fizzbuzzSum = sum . fizzbuzz
+
+main :: IO ()
+main = do
+  n <- readLn :: IO Int
+  print $ fizzbuzzSum n
