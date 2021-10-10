7dfe3f58080c86f73a6ed27ecb42967983482c5e
cutsea110 <cutsea110@gmail.com>
2021-10-10 09:54:12 +0900

    solved LEQ

--- ABC221/LEQ.hs
+++ ABC221/LEQ.hs
@@ -1,9 +1,9 @@
 {-# OPTIONS_GHC -O2 #-}
-{-# LANGUAGE TupleSections #-}
-{-# LANGUAGE DeriveFunctor #-}
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE FlexibleContexts #-}
 module Main where
+
+import Debug.Trace (trace)
 
 import Control.Arrow
 import Control.Monad
@@ -13,6 +13,7 @@ import Data.Bits
 import Data.Char
 import Data.Function (on)
 import Data.List
+import Data.STRef
 import qualified Data.Tree as T
 import qualified Data.Vector as V
 import qualified Data.Vector.Algorithms.Intro as Intro
@@ -73,7 +74,7 @@ getTuple = do
 -- Main
 ------------------------------------------------
 
-largeNum = 998244353
+modulo = 998244353
 
 getProblem' :: IO (Int, U.Vector Int)
 getProblem' = do
@@ -89,114 +90,83 @@ getProblem = do
 
 -- main :: IO ()
 main = do
-  (n, v) <- getProblem
-  let vs = V.fromList $ small v
-  let xs = V.ifoldl' f 0 vs
-  print xs
-    where f acc j (_, is) = (acc + foldl' g 0 is) `mod` largeNum
-            where g d i = (d + 2^(j-i-1)) `mod` largeNum
+  (_, v) <- getProblem'
+  let (n, xs) = monotone v
+  print $ solve n xs
 
+---------------------------------------------------
+-- BIT の (<>) も mod計算する
+instance Semigroup Int where
+  x <> y = (x + y) `mod` modulo
 
-small :: [Int] -> [(Int, [Int])]
-small xs = go (zip [0..] xs) t []
-  where
-    bw = bitWidth (10^9+10)
-    t = new bw
-    go []             t acc = zip xs (reverse acc)
-    go (ix@(i, x):xs) t acc = let acc' = query t x : acc
-                                  t' = inc x [i] t -- Monoid は [a]
-                              in go xs t' acc'
+instance Monoid Int where
+  mempty = 0
+---------------------------------------------------
 
-{- | 座標圧縮
+solve :: Int -> U.Vector Int -> Int
+solve n v = runST $ do
+  let (modInv', modPower') = (modInv modulo, modPower modulo)
+  let modPlus x y = (x + y) `mod` modulo
+
+  wk <- new n :: ST s (UM.MVector (PrimState (ST s)) Int)
+  result <- newSTRef 0
+
+  forM_ [0..(U.length v - 1)] $ \i -> do
+    let a = v U.! i
+    w <- sumTo wk a
+    modifySTRef' result (\ans -> (modPower' 2 i * w) `modPlus` ans)
+    inc a (modInv' (modPower' 2 (i+1))) wk
+
+  readSTRef result
+
+{- | 単調写像により 1-origin な整数に番号を振り直して、最大値も同時に返す
 -}
-compress :: (Num a, Ord a, UM.Unbox a) => U.Vector a -> U.Vector a
-compress vs = runST $ do
+monotone :: (Num a, Ord a, UM.Unbox a) => U.Vector a -> (a, U.Vector a)
+monotone vs = runST $ do
   let indexed = U.indexed vs
   let sorted = U.modify (Intro.sortBy (compare `on` snd)) indexed
   ss <- U.thaw sorted
-  comp ss 0 (0, 0)
+  (newMax, _origMax) <- comp ss (UM.length ss) 0 (0, 0)
   ss' <- U.freeze ss
   let restored = U.modify (Intro.sortBy (compare `on` fst)) ss'
-  return $ U.map snd restored
-
-{- |
-前提: v は snd について sort 済とする
--}
-comp vec i (ix, x) = do
-  let n = UM.length vec
-  if i > n-1 then return ()
-    else do
-      (j, y) <- UM.read vec i
-      let (ix', x') = if y > x then (ix+1, y) else (ix, x)
-      UM.modify vec (second $ const ix') i
-      comp vec (i+1) (ix', x')
-
--- UTILITY
-
-bitWidth :: Int -> Int
-bitWidth = length . toBit
-
-toBit :: Int -> [Int]
-toBit = reverse . unfoldr psi
+  return $ (newMax, U.map snd restored)
   where
-    psi n | n == 0 = Nothing
-          | otherwise = let (q, r) = n `divMod` 2
-                        in Just (r, q)
--- Bibary Indexed Tree
+    {- | 前提: v は snd について sort 済とする-}
+    comp vec n i (ix, x) = do
+      if i > n-1 then return (ix, x) -- (振りなおした最大値, オリジナルの最大値)
+        else do
+        (j, y) <- UM.read vec i
+        let (ix', x') = if y > x then (ix+1, y) else (ix, x)
+        UM.modify vec (second $ const ix') i
+        comp vec n (i+1) (ix', x')
 
-data BIT a = BIT Int (Tree a) deriving (Show, Functor)
 
-data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show, Functor)
-
-toForest :: Tree a -> T.Forest a
-toForest Empty = []
-toForest (Node x l r) = [T.Node x (toForest l ++ toForest r)]
-
-drawBIT :: Show a => BIT a -> IO ()
-drawBIT (BIT k t) = do
-  putStrLn (show k ++ " bits")
-  putStr . T.drawForest . fmap (fmap show) . toForest $ t
+---------------------------------------------
+-- Binary Indexed Tree
+---------------------------------------------
 
 {- |
-construct BIT on k bits. O(n)
+1-base です(0 は使用しない)
 -}
-new :: Monoid a => Int -> BIT a
-new k = BIT k $ f k
-  where f 0 = Empty
-        f k = Node mempty fk' fk' where fk' = f (k-1)
+new :: (PrimMonad m, Monoid a, UM.Unbox a)
+    => Int -> m (UM.MVector (PrimState m) a)
+new n = do
+  UM.replicate (n+1) mempty
 
-{- |
-construct BIT by maximum value n.
--}
-newByMax :: Monoid a => Int -> BIT a
-newByMax n = new k
-  where k = bitWidth n
+inc :: (PrimMonad m, Monoid a, UM.Unbox a)
+    => Int -> a -> UM.MVector (PrimState m) a -> m ()
+inc i w v = do
+  let n = UM.length v - 1 -- 最大値
+  if i > n then return ()
+    else do
+    UM.modify v (<>w) i
+    inc (i + (i .&. negate i)) w v
 
-{- |
-Increment the value at index i(1-base) by amount x. O(log n)
--}
-inc :: Monoid a => Int -> a -> BIT a -> BIT a
-inc i x (BIT k root) = BIT k $ f root (k-1) 0
-  where f Empty _ _ = error "invalid arguments"
-        f (Node y l r) j acc
-          | i `testBit` j =
-              if acc' == i
-                  then  y'   `seq` Node y' l r
-                  else  acc' `seq` Node y (f l j' acc') r
-          | otherwise = y'   `seq` Node y' l (f r j' acc)
-          -- NOTE: If y `mappend` x for List Monoid, then we should reverse to return value at lookup by (!).
-          where y' = x `mappend` y
-                j' = j-1
-                acc' = acc `setBit` j
-
-{- |
-Lookup the sum of all values from index 1 to index i. O(log n)
--}
-query :: Monoid a => BIT a -> Int -> a
-query (BIT k root) i = f root (k-1) mempty
-  where f Empty _ acc = acc
-        f (Node x l r) j acc
-          | i `testBit` j = acc' `seq` f l j' acc'
-          | otherwise     = f r j' acc
-          where j'   = j-1
-                acc' = acc `mappend` x
+sumTo :: (PrimMonad m, Monoid a, UM.Unbox a)
+      => UM.MVector (PrimState m) a -> Int -> m a
+sumTo v i = do
+  if i < 1 then return mempty
+    else do
+    x <- UM.read v i
+    y <- sumTo v (i - (i .&. negate i))
+    return (x <> y)
