ad2ec2a9324207eef7739eaf1458669c69c21fe1
cutsea110 <cutsea110@gmail.com>
2024-10-26 21:06:45 +0900

    new solve

---
+++ MATH/NumberOfMultiples1.hs
@@ -0,0 +1,32 @@
+module Main where
+
+import GHC.Real (toInteger)
+import Control.Arrow (first)
+import Data.Char (isSpace)
+import Data.List (unfoldr)
+import qualified Data.ByteString.Char8 as C
+
+type Parser a = C.ByteString -> Maybe (a, C.ByteString)
+
+parseInt :: Parser Integer
+parseInt = fmap (first toInteger) . C.readInt . C.dropWhile isSpace
+
+parseInt2 :: Parser (Integer, Integer)
+parseInt2 cs = case parseInt cs of
+  Just (x, cs') -> case parseInt cs' of
+    Just (y, cs'') -> Just ((x, y), cs'')
+    Nothing -> Nothing
+  Nothing -> Nothing
+
+getInts :: IO [Integer]
+getInts = unfoldr parseInt <$> C.getLine
+
+getTuple :: IO (Integer, Integer)
+getTuple = do
+  (x:y:_) <- getInts
+  return (x, y)
+
+
+main = do
+  n:x:y:_ <- getInts
+  print $ n `div` x + n `div` y - n `div` (x * y)
