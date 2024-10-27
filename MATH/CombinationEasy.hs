372e4f0cff44b5a60235ee20fcfcaaef89428d2a
cutsea110 <cutsea110@gmail.com>
2024-10-27 17:21:35 +0900

    new solve

---
+++ MATH/CombinationEasy.hs
@@ -0,0 +1,35 @@
+module Main where
+
+import GHC.Real (toInteger)
+import Control.Arrow (first)
+import Data.Char (isSpace)
+import Data.List (unfoldr)
+import qualified Data.ByteString.Char8 as C
+
+type Parser a = C.ByteString -> Maybe (a, C.ByteString)
+
+parseInt :: Parser Integer
+parseInt = fmap (first toInteger) . C.readInt . C.dropWhile isSpace
+
+parseInt2 :: Parser (Integer, Integer)
+parseInt2 cs = case parseInt cs of
+  Just (x, cs') -> case parseInt cs' of
+    Just (y, cs'') -> Just ((x, y), cs'')
+    Nothing -> Nothing
+  Nothing -> Nothing
+
+getInts :: IO [Integer]
+getInts = unfoldr parseInt <$> C.getLine
+
+getTuple :: IO (Integer, Integer)
+getTuple = do
+  (x:y:_) <- getInts
+  return (x, y)
+
+
+comb :: Integer -> Integer -> Integer
+comb n r = product [n-r+1..n] `div` product [1..r]
+
+main = do
+  n:r:_ <- getInts
+  print $ comb n r
