925c4c21e7dcece696afaada3048cda80a83ed04
cutsea110 <cutsea110@gmail.com>
2024-10-28 08:44:08 +0900

    new solve

---
+++ MATH/DiceExpectation.hs
@@ -0,0 +1,40 @@
+module Main where
+
+import GHC.Real (toInteger)
+import Control.Arrow (first)
+import Data.Char (isSpace)
+import Data.List (unfoldr)
+import qualified Data.ByteString.Char8 as C
+
+type Parser a = C.ByteString -> Maybe (a, C.ByteString)
+
+parseInt :: Parser Integer
+parseInt = fmap (first toInteger) . C.readInt . C.dropWhile isSpace
+
+parseInt2 :: Parser (Integer, Integer)
+parseInt2 cs = case parseInt cs of
+  Just (x, cs') -> case parseInt cs' of
+    Just (y, cs'') -> Just ((x, y), cs'')
+    Nothing -> Nothing
+  Nothing -> Nothing
+
+getInts :: IO [Integer]
+getInts = unfoldr parseInt <$> C.getLine
+
+getTuple :: IO (Integer, Integer)
+getTuple = do
+  (x:y:_) <- getInts
+  return (x, y)
+
+
+main = do
+  n <- head <$> getInts
+  bs <- getInts
+  rs <- getInts
+  print $ solve n bs rs
+
+average :: Integer -> [Integer] -> Double
+average n xs = fromIntegral (sum xs) / fromIntegral n
+
+solve :: Integer -> [Integer] -> [Integer] -> Double
+solve n bs rs = average n bs + average n rs
