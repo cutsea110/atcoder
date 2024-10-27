55d178d1d8fb94de0dc841059a2f69ed50e4e9f3
cutsea110 <cutsea110@gmail.com>
2024-10-28 08:44:18 +0900

    new solve

---
+++ MATH/AnswerExamRandomly.hs
@@ -0,0 +1,41 @@
+module Main where
+
+import GHC.Real (toInteger)
+import Control.Arrow (first)
+import Data.Char (isSpace)
+import Data.List (unfoldr)
+import qualified Data.ByteString.Char8 as C
+
+type Parser a = C.ByteString -> Maybe (a, C.ByteString)
+
+parseInt :: Parser Integer
+parseInt = fmap (first toInteger) . C.readInt . C.dropWhile isSpace
+
+parseInt2 :: Parser (Integer, Integer)
+parseInt2 cs = case parseInt cs of
+  Just (x, cs') -> case parseInt cs' of
+    Just (y, cs'') -> Just ((x, y), cs'')
+    Nothing -> Nothing
+  Nothing -> Nothing
+
+getInts :: IO [Integer]
+getInts = unfoldr parseInt <$> C.getLine
+
+getTuple :: IO (Integer, Integer)
+getTuple = do
+  (x:y:_) <- getInts
+  return (x, y)
+
+getTuples :: Integer -> IO [(Integer, Integer)]
+getTuples n = sequence $ replicate (fromIntegral n) getTuple
+
+main = do
+  n <- head <$> getInts
+  xs <- getTuples n
+
+  print $ solve xs
+
+solve :: [(Integer, Integer)] -> Double
+solve = foldl f 0.0
+  where
+    f acc (cs, pt) = acc + fromIntegral pt / fromIntegral cs
