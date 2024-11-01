de3db6544984f0d20a74f7bae5046f98b73ad1cd
cutsea110 <cutsea110@gmail.com>
2024-11-01 13:46:07 +0900

    AC for Knapsack1

--- MATH/Knapsack1.hs
+++ MATH/Knapsack1.hs
@@ -26,26 +26,26 @@ getIntegers = unfoldr parseInteger <$> C.getLine
 getIntVec :: Int -> IO (U.Vector Int)
 getIntVec n = U.unfoldrN n parseInt <$> C.getLine
 
-getTuple :: MonadFail m => m [a] -> m (a, a)
-getTuple p = do
-  (x:y:_) <- p
+getTuple :: IO (Int, Int)
+getTuple = do
+  (x:y:_) <- getInts
   return (x, y)
 
 getTuples :: (Monad m, Integral a) => m (a, a) -> a -> m [(a, a)]
 getTuples p n = replicateM (fromIntegral n) p
 
 main = do
-  (n, w) <- getTuple getInts
-  items <- getTuples (getTuple getInts) n
+  (!n, !w) <- getTuple
+  items <- U.replicateM n getTuple
 
   print $ solve (n, w) items
 
 type Weight = Int
 type Value = Int
 
-solve :: (Weight, Value) -> [(Weight, Value)] -> Value
-solve (n, w) = head . foldr f (replicate (w+1) 0)
-  where 
-    f (wi, vi) mem = zipWith max mem mem'
-      where !mem' =  map (+vi) (drop wi mem) ++ repeat 0
-
+solve :: (Int, Weight) -> U.Vector (Weight, Value) -> Int
+solve (n, w) = U.head . U.foldr f (U.replicate (w+1) 0)
+  where
+    f (wi, vi) mem = U.zipWith max mem mem'
+      where
+        mem' = U.map (+vi) (U.drop wi mem) U.++ U.replicate wi 0
