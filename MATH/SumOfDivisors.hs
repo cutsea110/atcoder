a03c354c1afc025f60f8fe2802609ae7aa480720
cutsea110 <cutsea110@gmail.com>
2024-11-04 15:07:38 +0900

    high performance!

--- MATH/SumOfDivisors.hs
+++ MATH/SumOfDivisors.hs
@@ -17,6 +17,16 @@ main = do
   print $ solve n
 
 solve :: Int -> Int
-solve n = foldl' f (n * succ n `div` 2) [2..n]
+solve n = foldl' f 0 [1..n]
   where
-    f acc i = acc + sum [i,i+i..n] -- for 1th
+    f acc i = acc + sumOn i
+    -- 等差数列の和を求める
+    -- i の倍数は n までに 1i, 2i, 3i, ... と n `div` i 個ある
+    -- これらがそれぞれの数の約数に含まれ、それぞれの数分の重みで寄与する
+    -- 例えば i = 3 なら 3, 6, 9, ... となるが、これはそれぞれ 3, 6, 9 に約数として含まれ、
+    -- かつそれぞれ 3, 6, 9 の重みで寄与するので単純に足し込む
+    -- 等差数列の和の公式で計算すれば O(1) で求まる
+    sumOn i = numOfTerms * (i + lastElem) `div` 2
+      where numOfTerms = n `div` i      -- 項数
+            lastElem   = i * numOfTerms -- 最後の項
+
